8326b3c7a9b7499119df466453155a25e34f9179
G3marchi <trimarchi.greyson@gmail.com>
2023-08-05 14:43:34 -0400

    a

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/tiw/',
-    bare: 'https://intoabyss.org/bare/',
+    bare: 'https://noctura.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
