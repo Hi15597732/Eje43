48f257bd0c849f8da238879348f328e072732e54
G3marchi <trimarchi.greyson@gmail.com>
2023-08-21 17:03:13 -0400

    a

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/uv/",
-  bare: "https://tomp.app",
+  bare: "https://noctura.app/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
