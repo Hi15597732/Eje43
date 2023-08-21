a3b8f4d1ac3820434e7eb64e2ad69ae538a82ccd
G3marchi <trimarchi.greyson@gmail.com>
2023-08-21 17:35:54 -0400

    Revert "a"
    This reverts commit 36a48693fa2ec163cfcdef11b4a6531bbb5367f4.

--- uv/uv.sw.js
+++ uv/uv.sw.js
@@ -359,6 +359,12 @@ EventEmitter.prototype._maxListeners = undefined;
 // added to it. This is a useful default which helps finding memory leaks.
 var defaultMaxListeners = 10;
 
+function checkListener(listener) {
+  if (typeof listener !== 'function') {
+    throw new TypeError('The "listener" argument must be of type Function. Received type ' + typeof listener);
+  }
+}
+
 Object.defineProperty(EventEmitter, 'defaultMaxListeners', {
   enumerable: true,
   get: function() {
@@ -777,4 +783,7 @@ function eventTargetAgnosticAddListener(emitter, name, listener, flags) {
       }
       listener(arg);
     });
-}}
+  } else {
+    throw new TypeError('The "emitter" argument must be of type EventEmitter. Received type ' + typeof emitter);
+  }
+}
